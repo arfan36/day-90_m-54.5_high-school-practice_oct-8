971f1b697f9cf679d02dd135605bf2b0e51c4ebd
arfan36 <arfanur36@gmail.com>
2022-10-10 11:56:04 +0600

    Hamburger menu

---
+++ src/components/Link/Link.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Link = ({ route }) => {
+    const { path, name } = route;
+    return (
+        <div>
+            <li>
+                <a href={path}>{name}</a>
+            </li>
+        </div>
+    );
+};
+
+export default Link;
